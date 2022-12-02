fcc7021c5c9aa7782ca1116aa88a08e3d8675d35
nataliajl <natalia.jleonardo@gmail.com>
2022-12-02 02:43:54 -0300

    feat: lamport clock

--- lamport.js
+++ lamport.js
@@ -1,3 +1,18 @@
 class Lamport{
-    
+    constructor (){
+        this.timestamp = 0;
+    }
+
+    tick(){
+        this.timestamp += 1;
+    }
+
+    tack(oddTimestamp){
+        if(oddTimestamp > this.timestamp){
+            this.timestamp = oddTimestamp;
+        }
+        this.tick();
+    }
 }
+
+export default Lamport;
