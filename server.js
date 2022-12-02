12b51ea4b0c26bd6d1b9c14bfb23f30dc0e7f5c3
nataliajl <natalia.jleonardo@gmail.com>
2022-12-02 02:44:15 -0300

    feat: add route

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
-const express = require("express");
-const https = require("https");
+import Lamport from "./lamport.js";
+import fetch from "node-fetch";
+import express from "express";
+import dotenv from 'dotenv';
+import http from "http";
+import os from 'os';
 
-(async () => {
-  const app = express().disable("x-powered-by");
-  const server = https.createServer(this.app);
-  const port = portServer || 3000;
+dotenv.config();
+
+(() => {
+  const clock = new Lamport();
+  const app = express();
+  const server = http.createServer(app);
+  const port = process.env.PORT || 9898;
   const router = express.Router();
-  app.use("/", router);
+
+  
+  router.get('/hello/:timestamp', async (req, res) => {
+    clock.tick();
+    console.log('Host: '+ os.networkInterfaces().lo[0].address +' - tickstamp: '+ clock.timestamp);
+    clock.tack(req.params.timestamp);
+    const communicate = await fetch(process.env.ADDRESS +'/hello/'+ clock.timestamp, {method: 'get'})
+                                    .then(function (response){
+                                      return response;
+                                    })
+                                    .catch(function (err){
+                                      console.error(err);
+                                    });
+    console.log('Host: '+ os.networkInterfaces().lo[0].address +' - tackstamp: '+ clock.timestamp);
+  });
+
+  app.use('/', router);
 
   server.listen(port, async function () {
-    logger.info(`App listening on http://localhost:${port}`);
+    console.log(`Listening on http://localhost:${port}`);
   });
+
 })();
