e0975f00d0c6723bf3f8317e4467212637d6985f
nataliajl <natalia.jleonardo@gmail.com>
2022-12-04 02:55:04 -0300

    refactor: create process object

---
+++ clock/process.js
@@ -0,0 +1,48 @@
+import schedule from 'node-schedule';
+import Lamport from './lamport.js';
+import logger from '../log.js';
+import fetch from "node-fetch";
+
+const clock = new Lamport();
+
+class Process {
+    constructor(name){
+      this.name = name;
+      this.processing = ms => new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async worker(timing){
+      clock.tick();
+      logger.info(`*TICK*: ${clock.timestamp}`);
+      await this.processing(timing); // simulação de tempo de processamento
+    }
+
+    startClock(){
+        schedule.scheduleJob(
+          '*/1 * * * * *',
+          async function () {
+            clock.tick();
+            logger.debug(`Local Clock: ${clock.timestamp}`);
+          }
+        );
+    };
+
+    startEvent(address){
+      clock.tick();
+      this.send(address);
+    }
+
+    event(timestamp, address) {
+      clock.tack(timestamp);
+      this.send(address);
+      logger.info(`**TACK**: ${clock.timestamp}`);
+    }
+
+    send(address){
+      fetch(`${address}/hello/${clock.timestamp}`, {method: 'GET'})
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+    }
+}
+
+export default Process;
--- clock/server.js
+++ clock/server.js
@@ -1,59 +1,37 @@
-import localClock from './localClock.js';
-import Lamport from "./lamport.js";
-import fetch from "node-fetch";
+import Process from './process.js';
+import logger from '../log.js';
 import express from "express";
 import dotenv from 'dotenv';
 import http from "http";
 
 dotenv.config();
-const clock = new Lamport();
-const local = localClock(clock);
 
-
-const processing = ms => new Promise(resolve => setTimeout(resolve, ms));
 (() => {
   const app = express();
+  const router = express.Router();
   const server = http.createServer(app);
   const port = process.env.PORT || 9898;
-  const router = express.Router();
+  const myProcess = new Process(process.env.PROCESS_NAME);
 
-  local.increment();
+  myProcess.startClock();
     
   // Lamport
-
   router.get('/hello', async (req, res) => {
-    clock.tick();
-    console.log('tick - '+ clock.timestamp);
-    const communicate = fetch(process.env.ADDRESS +'/hello/'+ clock.timestamp, {method: 'get'})
-                                    .then(function (response){
-                                      return response;
-                                    })
-                                    .catch(function (err){
-                                      console.error(err);
-                                    });
+    myProcess.startEvent(process.env.OTHER_PROCESS_ADDRESS);
+    res.json(`Greetings from ${myProcess.name}!`);
   });
 
-  //fazer o esquema de uma maquina central
   router.get('/hello/:timestamp', async (req, res) => {
-    clock.tick();
-    console.log('*tick* - '+ clock.timestamp);
-    await processing(process.env.PROCESS_TIME); // simulação de tempo de processamento
-    clock.tack(req.params.timestamp);
-    const communicate = fetch(process.env.OTHER_PROCESS_ADDRESS +'/hello/'+ clock.timestamp, {method: 'get'})
-                                    .then(function (response){
-                                      return response;
-                                    })
-                                    .catch(function (err){
-                                      console.error(err);
-                                    });
-    console.log('**TACK** - '+ clock.timestamp);  
+    myProcess.worker(process.env.PROCESS_TIME);
+    myProcess.event(req.params.timestamp, process.env.OTHER_PROCESS_ADDRESS);
+    res.json(`Greetings from ${myProcess.name}!`);
   });
 
   // Default
   app.use('/', router);
 
   server.listen(port, async function () {
-    console.log(`Listening on http://localhost:${port}`);
+    logger.info(`Listening on http://localhost:${port}`);
   });
 
 })();
